437c7cd680e94740157923da7e7cecceaae70786
allanimetamil <teenteenagepassanga@gmail.com>
2026-02-17 02:11:13 +0530

    dfsgf

--- Bybass.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const params = new URLSearchParams(window.location.search);
-    const next = params.get("reward");
-
-    if (next) {
-        // Normal browser fallback
-        window.location.replace(next);
-    }
-})();
